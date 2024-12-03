dedeacf4513c783b425a8bc1326ad87d6050846e
Tim Jarratt <tim.jarratt@maersk.com>
2024-12-03 11:05:25 +0100

    Use unix domain socket for test database

--- config/test.exs
+++ config/test.exs
@@ -9,6 +9,7 @@ config :elephant_carpacchio, ElephantCarpacchio.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
+  socket_dir: "/tmp/postgres",
   database: "elephant_carpacchio_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
