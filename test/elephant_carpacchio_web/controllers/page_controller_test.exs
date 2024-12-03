5eb1bd659d5b7eb65cd9acdc36b18cd61a6a4a76
Tim Jarratt <tim.jarratt@maersk.com>
2024-12-03 11:05:40 +0100

    Unit tests pass again

--- test/elephant_carpacchio_web/controllers/page_controller_test.exs
+++ test/elephant_carpacchio_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ElephantCarpacchioWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "TODO: lots of stuff"
   end
 end
